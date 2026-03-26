9908d65097cc265253637cf8ea83ee70c1285bfc
rob <rob@neurologyca.com>
2026-03-26 17:38:06 +0100

    Configure custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
-	site: isGitHubPages ? 'https://neurologyca.github.io' : undefined,
-	base: isGitHubPages ? '/api-dev-site' : '/',
+	site: 'https://api.kopernica.io',
+	base: '/',
 });
